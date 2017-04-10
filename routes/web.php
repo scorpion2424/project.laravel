db93f2a93ea1962ad5ba848510dbb68511ddfb9f
Bogdan <garfield1333@gmail.com>
2017-04-10 15:22:52 +0300

    Using Model, View and Controller

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,7 @@
 |
 */
 
-Route::get('/tasks', function () {
-    $tasks=DB::table('tasks')->latest()->get();
-    return view('tasks.index', compact('tasks'));
-});
 
-Route::get('/tasks/{task}', function ($id) {
+Route::get('/tasks','TasksController@index');
 
-    $task=DB::table('tasks')->find($id);
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks/{task}','TasksController@show');
